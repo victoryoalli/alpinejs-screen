b15f617a4ff510dbfa527ca34d4b81d508836f0a
Victor Yoalli <victoryoalli@gmail.com>
2021-08-21 15:54:03 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+export default function (Alpine) {
+    Alpine.directive('screen', (el, obj, { evaluateLater, effect, cleanup }) => {
+        let { value, expression, modifiers } = obj;
+        let evaluate = evaluateLater(expression);
+        let resizeObserver = new ResizeObserver(entries => {
+            entries.forEach(entry => {
+                evaluate((value) => {
+                    //console.log('condition', value)
+                })
+                modifiers.includes('once') && observer.disconnect()
+            })
+        })
+        resizeObserver.observe(el);
+        cleanup(() => observer.disconnect())
+    })
+}
